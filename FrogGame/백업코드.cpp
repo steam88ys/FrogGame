c35c5e83362bbf37ea3f1fc0d95ebf35c2715899
steam88ys <yssteam88@gmail.com>
2022-11-04 09:08:40 +0900

    점프 기능 성공

--- FrogGame/백업코드.cpp
+++ FrogGame/백업코드.cpp
@@ -18,19 +18,19 @@ int main(void)
 	RenderWindow app(VideoMode(WIDTH, HEIGHT), "FrogGame");
 	app.setFramerateLimit(60);
 
-	Texture t1, t2, t3, t4;
-	t1.loadFromFile("images/background.jpg");	// 배경화면 이미지
+	Texture bg1, bg2, t2, t3, t4;
+	bg1.loadFromFile("images/background.jpg");	// 배경화면 이미지1
+	bg2.loadFromFile("images/background.jpg");	// 배경화면 이미지2
 	t2.loadFromFile("images/frog.png");			// 개구리 이미지
 	t3.loadFromFile("images/wood.png");			// 발판 이미지
 	t4.loadFromFile("images/fly.png");			// 장애물 이미지
 
-	Sprite Background(t1), Frog(t2), Wood(t3), Fly(t4);
+	Sprite Background(bg1), Frog(t2), Wood(t3), Fly(t4);
 	Frog.setPosition(300, 400);
+	Wood.setPosition(300, 400);
+	Fly.setPosition(300, 400);
 
 	// 통나무 랜덤 위치로 생성하기 getPosition radom
-	Wood.setPosition( (rand()*(300-0)+300), (rand() * (400 - 0) + 400));
-	// 0~100		0~300
-
 
 	float dx = 5.0f, dy = 6.0f;	// (속도 조절 + 각도 조절)
 
@@ -51,23 +51,16 @@ int main(void)
 			Frog.move(9.0f, 0.0f);
 		if (Keyboard::isKeyPressed(Keyboard::Left))
 			Frog.move(-9.0f, 0.0f);
-
-		float gravity= (-18);	// 중력
-		float acc = 1;	// 가속도
-		
 		if (Keyboard::isKeyPressed(Keyboard::Up) || Keyboard::isKeyPressed(Keyboard::Space))
 		{
-
-			while (b.y > 0)
-			{
-				gravity += (acc*=3);
-				Frog.move(0.0f, gravity);
-			}
-
+			Frog.move(0.0f, -9.0f);
 			// 천천히 내려오게 하고 싶음 (끝까지 내려오게 해야함) -> 지금은 제자리
-
-			// 내려가는 값이 계속 변해야함 (중력, 떨어지는 정도를 변수로 처리)
-			// 값 변하는거 출력하면서 보기
+			Sleep(500);
+			while (b.y > 0)	// 끝까지 내려오지 않았을 때만 반복
+			{
+				Frog.move(0.0f, 18.0f);		// 밑으로 내려옴
+				Sleep(500);
+			}
 		}
 
 		if (b.x <= 0)    //왼쪽 벽 뚫지 못하게
@@ -89,7 +82,7 @@ int main(void)
 		app.clear();
 		app.draw(Background);
 		app.draw(Frog);
-		app.draw(Wood);
+		//app.draw(Wood);
 		//app.draw(Fly);
 		app.display();
 	}
